1f044651f33c76726904bc7654c4e9960e6bb94b
kojimay4243 <yuya02154243@gmail.com>
2025-03-08 20:45:05 +0900

    Remove ODrive ROS2 control plugin files and clean up related code

--- odrive_base/include/epoll_event_loop.hpp
+++ odrive_base/include/epoll_event_loop.hpp
@@ -3,58 +3,61 @@
 
 #include <sys/epoll.h>
 #include <sys/eventfd.h>
-#include <iostream>
-#include <functional>
-#include <vector>
 #include <unistd.h>
+
+#include <functional>
+#include <iostream>
+#include <vector>
 
 using std::placeholders::_1;
 using Callback = std::function<void(uint32_t)>;
 
-class EpollEventLoop {
+class EpollEventLoop
+{
 public:
-    struct EventContext {
-        int fd;
-        Callback callback;
-    };
+  struct EventContext
+  {
+    int fd;
+    Callback callback;
+  };
 
-    using EvtId = EventContext*;
+  using EvtId = EventContext *;
 
-    EpollEventLoop();
+  EpollEventLoop();
 
-    ~EpollEventLoop();
+  ~EpollEventLoop();
 
-    bool register_event(EvtId* p_evt, int fd, uint32_t events, const Callback& callback);
+  bool register_event(EvtId * p_evt, int fd, uint32_t events, const Callback & callback);
 
-    bool deregister_event(EvtId evt);
+  bool deregister_event(EvtId evt);
 
-    bool run_until_empty();
+  bool run_until_empty();
 
-    void drop_event(EvtId evt);
+  void drop_event(EvtId evt);
 
 private:
-    static constexpr size_t kMaxEventsPerIteration = 16;
-    int epollfd = -1;
-    size_t n_events_ = 0;
-    int n_triggered_events_ = 0;
-    struct epoll_event triggered_events_[kMaxEventsPerIteration];
+  static constexpr size_t kMaxEventsPerIteration = 16;
+  int epollfd = -1;
+  size_t n_events_ = 0;
+  int n_triggered_events_ = 0;
+  struct epoll_event triggered_events_[kMaxEventsPerIteration];
 };
 
-class EpollEvent {
+class EpollEvent
+{
 public:
-    bool init(EpollEventLoop* event_loop, const Callback& callback);
-    void deinit();
+  bool init(EpollEventLoop * event_loop, const Callback & callback);
+  void deinit();
 
-    bool set();
+  bool set();
 
 private:
-    void on_trigger(uint32_t event_id);
+  void on_trigger(uint32_t event_id);
 
-    EpollEventLoop* event_loop_;
-    int fd_ = -1;
-    EpollEventLoop::EventContext* evt_;
-    Callback callback_;
+  EpollEventLoop * event_loop_;
+  int fd_ = -1;
+  EpollEventLoop::EventContext * evt_;
+  Callback callback_;
 };
 
-
-#endif // EPOLL_EVENT_LOOP_HPP
+#endif  // EPOLL_EVENT_LOOP_HPP
